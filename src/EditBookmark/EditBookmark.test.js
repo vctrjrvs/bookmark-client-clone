c2c55343aa8bc6e186e9e30151f0c83c0454d6ea
Victor Jarvis <vctrjrvs@gmail.com>
2020-06-18 11:28:54 -0400

    added changes

--- src/EditBookmark/EditBookmark.test.js
+++ src/EditBookmark/EditBookmark.test.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom';
 import EditBookmark from './EditBookmark';
 
 it('renders without crashing', () => {
-     const div = document.createElement('div');
-     const props = {
-          match: { params: {} },
-          history: {
-               push: () => { }
-          },
-     }
-     ReactDOM.render(<EditBookmark {...props} />, div);
-     ReactDOM.unmountComponentAtNode(div);
+  const div = document.createElement('div');
+  const props = {
+    match: { params: {} },
+    history: {
+      push: () => {}
+    },
+  }
+  ReactDOM.render(<EditBookmark {...props} />, div);
+  ReactDOM.unmountComponentAtNode(div);
 });
